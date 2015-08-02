3e3399ab535184c9f8484fb013ed1d2bc3d6a5b2
anthonykoch <kindaswiss@gmail.com>
2015-08-01 20:16:05 -0700

    Update README.md, overall clean up, remove jquery, lodash

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
 # Splash Page and Modal
 A fun exercise by [DevTips](https://github.com/DevTips). 
 
-A live preview of my version can be seen here [seen here](http://kindaswiss.github.io/splash-page-and-modal/). 
+A live preview of my version can be seen here [seen here](http://anthonykoch.github.io/splash-page-and-modal/). 
 
 
 ## Goals
-The first commit is my first version. For the second version, I had some goals. 
 
 - Support IE 9+
+- Make it responsive 
 - Integrate concepts from ["Making modal windows better for everyone"](http://www.smashingmagazine.com/2014/09/15/making-modal-windows-better-for-everyone/) by SmashingMagazine 
 - Use some cool animations/transitions  
 - Allow only elements within the modal to be tabbable when the modal is visible 
 - Disallow tabbing to the modal when it is hidden 
-- Resume focus to the last active element when the modal closes 
 
 ## Problems
 - On smaller devices, the modal is pushed outside of the overlay, causing the background to peek through.
 
 ## Tools I Used
-- [VueJS](http://vuejs.org/) for JS
-- [JQuery](https://jquery.com/) for DOM manipulation
-- [Sass](http://sass-lang.com/guide) indented
+- [VueJS](http://vuejs.org/) for JS 
+- [Sass](http://sass-lang.com/guide), the indented syntax 
 - [BEM](http://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax/) for CSS class names
 - [Gulp](https://github.com/gulpjs/gulp) For processing Sass files and linting JS
 - [Modernizr](http://modernizr.com/) used a snippet of code from modernizr to detect if pointerevents are supported 
@@ -30,11 +28,8 @@ The first commit is my first version. For the second version, I had some goals.
 
 - Transitions don't work properly when the property doesn't exist beforehand. For example, if transitioning the left property on hover, the left property needs to be defined on the `selector` as well as `selector:hover`
 - Chrome will allow a unitless value for the `perspective` property and Firefox and IE won't. 
-- Focusing or moving an element when it is being transitioned will cause the transition to fail. 
+- Focusing, moving, or hiding an element when it is being transitioned will cause the transition to fail. 
 - VueJS is awesome. 
 - Sass is awesome 
 - [DevTips](https://www.youtube.com/user/DevTipsForDesigners) is awesome 
-
-
-
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,80 +1,69 @@
 
 (function () {
-'use strict';
-
-
-var ui = {
-	attached: function () {
-		this.ui = this.$$;
-	}
-};
-
-/**
- * Listens to the events specified on the window object. 
- * Multiple methods can be added by separating them with 
- * a comma. The keys should be the event and should map to 
- * methods on the vue object. 
- * 
- * Usage:
- * data: {
- * 		windowEvents: {
- * 			keydown: 'onKeyDown',
- * 			focusin: 'checkFocus, refocus'
- * 		}
- * } 
- * 
- * @type {Object}
- */
-var windowEvents = {
-	attached: function () {
-		if (typeof jQuery !== 'function') {
-			var err = new Error('jui: jQuery is not defined');
-			throw err;
-		}
-		var $window = jQuery(window);
-
-		/**
-		 * Loop through each of the event names in the windowEvents 
-		 * object and find the associated method function and 
-		 * add it as a listener to the window. 
-		 */
-		
-		_.forIn(this.windowEvents, function (methodNames, eventName) {
-			methodNames = _.map(this.windowEvents[eventName].split(','), _.trim),
-			
-			_.each(methodNames, function (methodName) {
-				/**
-				 * The method on the Vue object
-				 * @name method
-				 * @type {Function}
-				 */
-				var method = this[methodName];
-
-				// If  no method is found, throw an error 
-				if (typeof method !== 'function') {
-					var err = new Error('Vue windowEvents mixin: No method could be found for name ' + methodName);
-					throw err;
-				}
-
-				$window.on(eventName, method);
-			}, this);
-
-		}, this);
-		
-
-	}
-};
-
-
-
-$(function () {
 	'use strict';
+
 	var ESCAPE_KEY = 27;
+
+
+
+	/**
+	 * Listens to events on the window object and fires the
+	 * method specified. Multiple methods can be added by separating them with
+	 * a comma. The keys should be the event and should map to
+	 * methods on the vue object.
+	 *
+	 * Usage:
+	 * data: {
+	 * 		windowEvents: {
+	 * 			keydown: 'onKeyDown',
+	 * 			click: 'doSomething, anotherThing'
+	 * 		}
+	 * }
+	 *
+	 * @type {Object}
+	 */
+	var windowEvents = {
+		attached: function () {
+
+			/**
+			 * Loop through each of the event names in the windowEvents
+			 * object and find the associated method function and
+			 * add it as a listener to the window.
+			 */
+			for (var eventName in this.windowEvents) {
+				var methodNames = this.windowEvents[eventName];
+
+				// get each of the method names that should be called and trim the name
+				methodNames = this.windowEvents[eventName].split(',').map(function (methodName) {
+					return methodName.trim();
+				});
+
+				methodNames.forEach(function (methodName) {
+					/**
+					 * The method on the Vue object
+					 * @name method
+					 * @type {Function}
+					 */
+					var method = this[methodName];
+
+					// If  no method is found, throw an error
+					if (typeof method !== 'function') {
+						var err = new Error('Vue windowEvents mixin: No method could be found for name ' + methodName);
+						throw err;
+					}
+
+					window.addEventListener(eventName, method, false);
+				}, this);
+			}
+		}
+	};
+
+
 
 
 	/**
 	 * https://github.com/Modernizr/Modernizr/pull/924/files
-	 * Does this browser support csspointerevents? 
+	 * Does this browser support csspointerevents?
 	 * @type  {Boolea}
 	 */
 	var csspointerevents = (function () {
@@ -84,98 +73,99 @@ $(function () {
 	}());
 
 
-	var overlay = window.overlay = new Vue({
+
+
+	var overlay = new Vue({
 		el: '#modal-overlay',
 
-
-		mixins: [ui, windowEvents],
-
-
-		data: {
-			tabIndex: '-1',
-			ariaHidden: 'true',
-
-
-			isHidden: true,
-			/**
-			 * Whether or not the modal is showing 
-			 * @type {Boolean}
-			 */
-			showing: false,
-
-
-			/**
-			 * Saves the last focused element before the modal was opened 
-			 * so it can be focused again when the modal closes 
-			 * @type {Element}
-			 */
-			lastActive: null,
-
-
-			/**
-			 * Listen to these events on window 
-			 */
-			windowEvents: {
-				keydown: 'escape',
-				focusin: 'checkFocus'
-			},
+		mixins: [windowEvents],
+		attached: function () {
+			this.ui = this.$$;
 		},
+		data: function () {
+			return {
+				tabIndex: '-1',
 
+				ariaHidden: 'true',
+
+				/**
+				 * Whether or not the modal is hidden with "display: none".
+				 * The modal is normally hidden with "pointer-events: none"
+				 * and "opacity: 0". If pointer events are not supported,
+				 * "display: none" is used, which causes the transition to
+				 * not work properly.
+				 * @type {Boolean}
+				 */
+				isHidden: true,
+
+				/**
+				 * Whether or not the modal is is showing.
+				 * @type {Boolean}
+				 */
+				isShowing: false,
+
+				/**
+				 * Saves the last focused element before the modal was opened
+				 * so it can be focused again when the modal closes
+				 * @type {Element}
+				 */
+				lastActive: null,
+
+				/**
+				 * Listen to these events on window
+				 */
+				windowEvents: {
+					keydown: 'escape',
+					focusin: 'checkFocus'
+				},
+			}
+		},
 
 		methods: {
 
-			
 			/**
-			 * Hide the modal 
-			 * 
-			 * @return {void} 
+			 * Hide the modal and focus back to the element that was
+			 * focused before the modal was opened.
+			 * @return {void}
 			 */
 			hide: function(event) {
+				// "event" may be undefined from calling this.hide manually
+				if ( ! event || ~ [this.$el, this.ui.close].indexOf(event.target)) {
 
-				if ( ! event || ~[this.$el, this.ui.close].indexOf(event.target)) {
-
-					/**
-					 * If CSS pointer events are not supported the element 
-					 * will be hidden. 
-					 */
+					// If CSS pointer events are not supported, the element will
+					// be set to "display: none", which messes up the transition.
 					if ( ! csspointerevents) {
 						this.isHidden = true;
 					}
-					this.showing = false;
-					/**
-					 * Resume focus to the last active element before the modal 
-					 * was opened 
-					 */
+
+					this.isShowing = false;
+
+					// Resume focus to the last active element before the modal was opened
 					this.lastActive.focus();
 					this.tabIndex = '-1';
 					this.ariaHidden = 'true';
 				}
 			},
 
-
 			/**
-			 * Show the modal and focus it so that tabbing will focus the 
-			 * modal's exit button. 
-			 * 
-			 * @return {void} 
+			 * Show the modal and focus it so that tabbing will focus the
+			 * modal's exit button.
+			 * @return {void}
 			 */
 			show: function() {
 				this.isHidden = false;
-				this.showing = true;
+				this.isShowing = true;
 				this.lastActive = document.activeElement;
 				this.$nextTick(function () {
 					this.tabIndex = '0';
 					this.$el.focus()
 					this.ariaHidden = 'false';
 				});
-
 			},
 
-
 			/**
-			 * When the submit button is pressed, hide the modal 
-			 * 
-			 * @param  {Event} event 
+			 * When the submit button is pressed, hide the modal
+			 * @param  {Event} event
 			 * @return {void}
 			 */
 			submit: function (event) {
@@ -183,46 +173,45 @@ $(function () {
 				this.hide();
 			},
 
-
 			/**
-			 * Hide the modal when the escape button is pressed 
-			 *
-			 * @param {KeydownEvent} event 
-			 * @return {void} 
+			 * Hide the modal when the escape button is pressed
+			 * @param {Event} event
+			 * @return {void}
 			 */
 			escape: function (event) {
-				if (this.showing && event.which === ESCAPE_KEY) {
+				if (this.isShowing && event.which === ESCAPE_KEY) {
 					this.hide();
 				}
 			},
 
-
 			/**
-			 * Check if the modal has lost focus to an element 
-			 * outside of it. If it has, refocus the modal. 
-			 * 
-			 * @param  {FocusinEvent} event 
-			 * @return {void}       
+			 * Check if the modal has lost focus to an element
+			 * outside of it. If it has, refocus the modal.
+			 * @param  {Event} event
+			 * @return {void}
 			 */
 			checkFocus: function (event) {
 				var overlay = this.$el;
-				var contains = $.contains(overlay, event.target) || event.target === overlay;
-				if (this.showing && ! contains) {
+				var contains = overlay.contains(event.target) || event.target === overlay;
+
+				if (this.isShowing && ! contains) {
 					overlay.focus();
 				}
-			}, 
+			},
 
 			/**
-			 * Make the overlay `display: none` so that when the 
-			 * element can't be tabbed to after it is transitioned 
-			 * its opacity back to 0. 
-			 * 
-			 * @param  {TransitionEndEvent} event 
-			 * @return {void} 
+			 * Make the overlay `display: none` so that when the
+			 * element can't be tabbed after it is set to "opacity: 0"
+			 * @param  {Event} event
+			 * @return {void}
 			 */
-			transitionend: function (event) {
-				if (event.target !== this.$el) { return; }
-				if ( ! that.showing) {
+			onTransitionend: function (event) {
+				if (event.target !== this.$el) {
+					return;
+				}
+
+				if ( ! this.isShowing) {
+					// Set css display to none
 					this.isHidden = true;
 				}
 			}
@@ -230,20 +219,15 @@ $(function () {
 	});
 
 
+
+
 	/**
-	 * Whenever the learn more button is clicked, 
-	 * show the suscribe modal dialog box 
+	 * Whenever the learn more button is clicked,
+	 * show the suscribe modal dialog box
 	 */
-	new Vue({
-		el: '#learn-more',
-		methods: {
-			onClick: function () {
-				overlay.show();
-			}
-		}
-	});
+	document.querySelector('#learn-more')
+		.addEventListener('click', function () {
+			overlay.show();
+		});
 
-});
-
-
-}()); // wrapping iife
+}());
